a636515da67c39bc3ea07307768b2c1886482982
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-04-27 01:22:25 -0500

    feat(script): add language section

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,7 @@ const spa = document.querySelector('.es');
 const en = document.querySelector('.en');
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav .nav-links a");
+var check=document.querySelector(".check");
 
 function changedcolor() {
     if (barra.checked) {
@@ -38,3 +39,21 @@ navLinks.forEach((link) => {
     }
     });
 });
+
+//agregar un cuadro al hacer scroll
+window.addEventListener("scroll", function(){
+    var nav=document.querySelector("nav");
+    nav.classList.toggle("abajo",window.scrollY>0);
+})
+
+//traduccion
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id=check.checked;
+    if(id==true){
+        location.href="assets/language/index-en.html"
+    }else{
+        location.href="../../index.html"
+    }
+}
